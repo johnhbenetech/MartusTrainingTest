0d4a64afb9774cd8e4dfea0d8ed75935383bfef6
John Higgins <johnh@Benetech.local>
2016-05-23 13:21:24 -0700

    Test Tour

---
+++ my_first_tour.js
@@ -0,0 +1,16 @@
+ // Define the tour!
+    var tour = {
+      id: "hello-hopscotch",
+      steps: [
+        {
+          title: "My Header",
+          content: "This is the header of my page.",
+          target: "link1",
+          placement: "right"
+        }
+      ]
+    };
+
+    // Start the tour!
+    hopscotch.startTour(tour);
+        
