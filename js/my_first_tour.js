982fae8d49d566335edc15991c5ad01b189e842f
John Higgins <johnh@Benetech.local>
2016-05-24 15:08:35 -0700

    xhr

--- js/my_first_tour.js
+++ js/my_first_tour.js
@@ -9,6 +9,11 @@
           placement: "bottom",
 		  onNext: function() {
 			var xhr = new XMLHttpRequest();  
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == XMLHttpRequest.DONE) {
+					alert(xhr.responseText);
+				}
+			}
 			xhr.open("POST", "https://paste.ee/api", true);
 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 			xhr.send({"key":"0dedeb9f5699ef19ae6ff4f12ff8e694","paste":"Moved to next step"});
